e340abca6a4bc700af19aec872a7ce80d70717b8
srishree <srishree546@gmail.com>
2016-01-10 13:17:35 +0530

    Revert "exception3"
    This reverts commit f806a7bfc570f04ca93fefa24bc0a946a2875962.

--- src/com/shree/exception/TestException.java
+++ src/com/shree/exception/TestException.java
@@ -5,21 +5,16 @@ public class TestException {
 	public static void main(String []args) {
 		int x=0;
 		int y=12;
-		TestE obj=new TestE();
-		z=obj.division(x,y);
+		z=TestException.division(x,y);
 		System.out.println(z);
 	}
-}
 
-class TestE {
-	int z;
-	public int division(int x,int y){
+	public static int division(int x,int y){
 		try{
 			z=y/x;
 			}catch(ArithmeticException e)
-			{		
-				System.out.println("division by zero not allowed "+e);
-			}
-		return z;			
+			{}
+		return z;
+			
 	}
 }

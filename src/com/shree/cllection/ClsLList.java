821405a137d173c54664460123a774450459e352
srishree <srishree546@gmail.com>
2016-01-16 18:52:20 +0530

    Llist

---
+++ src/com/shree/cllection/ClsLList.java
@@ -0,0 +1,19 @@
+package com.shree.cllection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ClsLList {
+	public static void main(String []args) {
+		List<Integer> ll=new LinkedList<Integer>();
+		ll.add(11);
+		ll.add(150000);
+		ll.add(11);
+		ll.add(150000);
+		ll.add(110000);
+		ll.add(15);
+	System.out.println(ll.size());	
+	}
+
+	
+}

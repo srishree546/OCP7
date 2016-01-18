50646b1e24328ef6c72ac4dc963a194a8ab9fde1
SB <SB@192.168.2.2>
2016-01-18 11:29:05 -0800

    What will be the output ?

---
+++ src/com/shree/thread/Worker.java
@@ -0,0 +1,53 @@
+package com.shree.thread;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Worker {
+
+    private int count = 0;
+
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        worker.doWork();
+    }
+
+    public synchronized void increment(String threadName) throws InterruptedException {
+        count++;
+    }
+
+    public void doWork() {
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        increment(Thread.currentThread().getName());
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        });
+        thread1.start();
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        increment(Thread.currentThread().getName());
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        });
+        thread2.start();
+        
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException ignored) {}
+        // What will be the output ?
+        System.out.println("Count is: " + count);
+    }
+}
